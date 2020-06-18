80fd4c5f44d6869a73e9cdeaa4d1adda32c8fbc2
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-06-18 11:15:56 -0300

    cap 2 completo

--- app/src/main/java/br/com/alura/financask/ui/adapter/ListaTransacoesAdapter.kt
+++ app/src/main/java/br/com/alura/financask/ui/adapter/ListaTransacoesAdapter.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import br.com.alura.financask.R
 import br.com.alura.financask.model.Transacao
+import kotlinx.android.synthetic.main.form_transacao.view.*
 import kotlinx.android.synthetic.main.transacao_item.view.*
+import java.text.SimpleDateFormat
 
 class ListaTransacoesAdapter(
     transacoes: List<Transacao>,
@@ -22,7 +24,13 @@ class ListaTransacoesAdapter(
             .from(context).inflate(R.layout.transacao_item, parent, false)
 
         val transacao = transacoes[posicao]
-        viewCriada.transacao_valor.setText(transacao.getValor().toString())
+        viewCriada.transacao_valor.text = transacao.valor.toString()
+        viewCriada.transacao_categoria.text = transacao.categoria
+
+        val formatadoBrasileiro = "dd/MM/yyyy"
+        val format = SimpleDateFormat(formatadoBrasileiro)
+        val dataFormatada = format.format(transacao.data.time)
+        viewCriada.transacao_data.text = dataFormatada
 
         return viewCriada
     }
