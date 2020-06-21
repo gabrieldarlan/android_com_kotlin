831e4e6040585ea170a6517ea903fbb55be48428
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-06-21 18:07:27 -0300

    cap 02

--- app/src/main/java/br/com/alura/financask/ui/ResumoView.kt
+++ app/src/main/java/br/com/alura/financask/ui/ResumoView.kt
@@ -11,8 +11,8 @@ import kotlinx.android.synthetic.main.resumo_card.view.*
 import java.math.BigDecimal
 
 class ResumoView(
-    private val context: Context,
-    private val view: View,
+    context: Context,
+    private val view: View?,
     transacoes: List<Transacao>
 ) {
 
@@ -28,28 +28,33 @@ class ResumoView(
 
     private fun adicionaReceita() {
         val totalReceita = resumo.receita
-        with(view.resumo_card_receita) {
-            setTextColor(corReceita)
-            text = totalReceita.formataParaBrasileiro()
+        view?.let {
+            with(it.resumo_card_receita) {
+                setTextColor(corReceita)
+                text = totalReceita.formataParaBrasileiro()
+            }
         }
     }
 
 
     private fun adicionaDespesa() {
         val totalDespesa = resumo.despesa
-        with(view.resumo_card_despesa) {
-            setTextColor(corDespesa)
-            text = totalDespesa.formataParaBrasileiro()
+        view?.let {
+            with(it.resumo_card_despesa) {
+                setTextColor(corDespesa)
+                text = totalDespesa.formataParaBrasileiro()
+            }
         }
     }
 
     private fun adicionaTotal() {
         val total = resumo.total
         val cor = corPor(total)
-
-        with(view.resumo_card_total) {
-            setTextColor(cor)
-            text = total.formataParaBrasileiro()
+        view?.let {
+            with(it.resumo_card_total) {
+                setTextColor(cor)
+                text = total.formataParaBrasileiro()
+            }
         }
     }
 
